d17aeb8580f400369bbb5e669ba4153941db5307
SIDDARTH <kgsiddarth1@gmail.com>
2026-04-21 09:46:04 +0530

    node-js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const server=http.createServer((req,res) => {
     console.log(req.url);
     res.end("Hello World");
 
+}
+if(req.url === "/"){
+    res.end("home page");
+}
+elseif(req.url)
 });
 server.listen(3002, () => {
     console.log("server is running on port 3002");
